9f2a07fa18443ba15da1d65d2eb987720e32f0da
AndreaMercanti <andrea.mr@hotmail.it>
2020-06-28 21:22:16 +0200

    Modify the structure by adding a co-fetcher class
    The new class helps the spider to correctly fetch reviews for each film.
    
    fimlID and filmTitle attributes changed quicker than the request were yielded, so it resulted to link some reviews to one film, instead of the correct one.
    Get rid of this problem by shifting the parsing method to the spider's helper with a copy reference of the film's ID and title, and the url request, so that each instance of the helper refers to a single film in the dataset.

--- scraping/scraping/spiders/fetcher.py
+++ scraping/scraping/spiders/fetcher.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from ..items import ScrapingItem
 import json
-from datetime import datetime
+from .spider_helper import SpiderHelper
 from ..db import DBManager
 from sqlalchemy.exc import IntegrityError
 
@@ -12,49 +11,21 @@ class FetcherSpider(scrapy.Spider):
     name = 'fetcher'
     allowed_domains = ['imdb.com']
 
-    def __init__(self, **kwargs):
-        self.ajax_url = None
-        self.filmID = None
-        self.filmTitle = None
-        super().__init__(**kwargs)
-
     def start_requests(self):
         with open(DATASET_PATH, 'r') as file:
             for line in file.readlines():
                 movie = json.loads(line)
-                self.filmID = movie['imdbID']
-                self.filmTitle = movie['Title']
+                
+                film_id = movie['imdbID']
+                film_title = movie['Title']
+                ajax_url = 'https://www.imdb.com/title/{}/reviews/_ajax?sort=userRating&dir=desc&ratingFilter=0'.format(film_id)
+                
+                helper = SpiderHelper(ajax_url, film_id)
+                
                 dbMgr = DBManager.getInstance()
                 try:
-                    dbMgr.addFilm(self.filmID, self.filmTitle)
+                    dbMgr.addFilm(film_id, film_title)
                 except IntegrityError:
                     print('The film already exists')
-                self.ajax_url = 'https://www.imdb.com/title/{}/reviews/_ajax?sort=userRating&dir=desc&ratingFilter=0'.format(self.filmID)
-                yield scrapy.Request(self.ajax_url, callback=self.parse, dont_filter=True)
-
-    def parse(self, response):
-        item = ScrapingItem()
-        reviews = response.css('.collapsable')
-        
-        for review in reviews:
-            user = review.css(".display-name-link a::text").get()
-            rating_components = review.css(".rating-other-user-rating span::text").extract()
-            date = review.css(".review-date::text").get()
-            review_components = review.css(".show-more__control::text").extract()
-
-            # PARSING AND BETTER FORMATTING THE DIFFERENT ELEMS
-            date = datetime.strptime(date, '%d %B %Y').date() # creating a date object
-            review_str = ''.join(review_components[0:-2]) # compacting all the review components into one string, but the last two
-            rating = ''.join(rating_components) # compacting all rating components into one string
-            
-            item['filmID'] = self.filmID
-            item['user'] = user
-            item['rating'] = rating
-            item['date'] = date
-            item['review'] = review_str
-            
-            yield item
-        
-        next_page = response.css(".load-more-data").xpath('@data-key').get()
-        if next_page is not None:
-            yield scrapy.Request(self.ajax_url + '&ref_=undefined&paginationKey=' + next_page, callback=self.parse)
+                
+                yield scrapy.Request(ajax_url, callback=helper.parse, dont_filter=True)
